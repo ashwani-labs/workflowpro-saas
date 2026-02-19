ac4b9694a197f41e5407ffbeb1970553c5f247f6
Ashwani Sharma <ashwani.k.sharma2001@gmail.com>
2026-02-19 23:36:34 +0530

    Setup Spring Boot backendSetup Spring Boot backend

---
+++ backend/src/test/java/com/workflowpro/WorkflowproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflowpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
